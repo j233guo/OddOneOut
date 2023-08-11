6c91c88ff4fda303d2c6a235b022da9ac3bb8760
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2023-08-11 18:14:29 -0400

    Initial Commit

---
+++ OddOneOut/OddOneOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddOneOutApp.swift
+//  OddOneOut
+//
+//  Created by Jiaming Guo on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct OddOneOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
